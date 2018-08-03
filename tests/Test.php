76a5b62c4296c559ad22636cac71772720870f0c
choyunsung <steven@wemakeprice.com>
2018-08-03 18:46:44 +0900

    over int cal

--- tests/Test.php
+++ tests/Test.php
@@ -8,7 +8,7 @@
 
 include '/Volumes/M2HDD/workspaces/myprojects/calc_github/vendor/autoload.php';
 echo PHP_INT_MAX."<br><br>";
-echo "+:    ".steven\calc('11111111111111.10987654321 + 111111.22',8)."<br>";
+echo "+:    ".steven\calc('111111111111111.10987654321 + 111111.22',8)."<br>";
 echo "/:    ".steven\calc('111111111.1111111111 / 111111111.22',8)."<br>";
 echo "*:    ".steven\calc('111111.1111111111 * 1111111.22',8)."<br>";
 echo "-:    ".steven\calc('111111111.1111111111 - 111111111.22',8)."<br>";
